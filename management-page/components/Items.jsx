ab6ea149180b5fc94e4c20001e876a294b6e3ddf
krisawyerr <krisawyerrr@gmail.com>
2024-07-02 16:55:24 -0700

    cleaned up code

--- management-page/components/Items.jsx
+++ management-page/components/Items.jsx
@@ -38,8 +38,6 @@ export default function Items({setPickedItem}) {
         fetchItems()
     } 
 
-    console.log(items)
-
     return (
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
