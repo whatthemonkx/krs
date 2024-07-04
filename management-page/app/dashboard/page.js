06c16783d9d58c83bee8049179196aee1a70dd4b
krisawyerr <krisawyerrr@gmail.com>
2024-07-04 11:24:25 -0700

    fixed pages

--- management-page/app/dashboard/page.js
+++ management-page/app/dashboard/page.js
@@ -25,6 +25,7 @@ function Dashboard() {
   const recentSale = sales && [...sales].reverse().slice(0, 6);
   const { currentUser, logout } = useContext(AuthContext);
   const router = useRouter();
+  const [hydrated, setHydrated] = useState(false);
 
   async function handleLogout(e) {
     e.preventDefault();
@@ -40,6 +41,8 @@ function Dashboard() {
     if (!currentUser) {
       router.push("/");
     }
+
+    setHydrated(true);
   }, [currentUser, router]);
   
   useEffect(() => {
@@ -91,6 +94,10 @@ function Dashboard() {
     fetchItems()
   }, []);
 
+  if (!hydrated) {
+    return null; 
+  }  
+
   function formatToDollar(amount) {
     return new Intl.NumberFormat('en-US', {
       style: 'decimal',
@@ -110,7 +117,7 @@ function Dashboard() {
 
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <header className="sticky z-[1000] top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
+      <div className="sticky z-[1000] top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
         <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
           <Link
             href="/dashboard"
@@ -192,7 +199,7 @@ function Dashboard() {
             </DropdownMenuContent>
           </DropdownMenu>
         </div>}
-      </header>
+      </div>
 
       {currentUser && <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
         <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
