f1b5c5448061138431519a76374bf81dd2e3a57e
krisawyerr <krisawyerrr@gmail.com>
2024-07-04 02:28:41 -0700

    switch server to live server

--- api/db.js
+++ api/db.js
@@ -3,17 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'matheson',
-    database: 'store'
-})
+// export const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'matheson',
+//     database: 'store'
+// })
 
-/* 
+
 export const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DBNAME
-}) */
+})
