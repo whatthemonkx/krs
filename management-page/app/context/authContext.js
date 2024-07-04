5d6bc0be98ab5b2685c32c4e4f8de4693f8b2bb2
krisawyerr <krisawyerrr@gmail.com>
2024-07-04 02:45:21 -0700

    fix deployment issue

--- management-page/app/context/authContext.js
+++ management-page/app/context/authContext.js
@@ -3,30 +3,47 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
-export const AuthContext = createContext()
+export const AuthContext = createContext();
 
-export const AuthContextProvider = ({children}) => {
+export const AuthContextProvider = ({ children }) => {
     const API_URL = process.env.NEXT_PUBLIC_SERVER_LINK;
 
-    const [currentUser, setCurrentUser] = useState(JSON.parse(localStorage.getItem("user") || null));
+    const [currentUser, setCurrentUser] = useState(() => {
+        if (typeof window !== 'undefined') {
+            return JSON.parse(localStorage.getItem("user")) || null;
+        }
+        return null;
+    });
 
-    const login = async(input) => {
-        const res = await axios.post(`${API_URL}/auth/login`, input);
-        setCurrentUser(res.data)
-    }
+    const login = async (input) => {
+        try {
+            const res = await axios.post(`${API_URL}/auth/login`, input);
+            setCurrentUser(res.data);
+        } catch (error) {
+            console.error("Login failed", error);
+        }
+    };
 
-    const logout = async(input) => {
-        const res = await axios.post(`${API_URL}/auth/logout`);
-        setCurrentUser(null)
-    }
+    const logout = async () => {
+        try {
+            await axios.post(`${API_URL}/auth/logout`);
+            setCurrentUser(null);
+        } catch (error) {
+            console.error("Logout failed", error);
+        }
+    };
 
     useEffect(() => {
-        localStorage.setItem("user", JSON.stringify(currentUser))
-    }, [currentUser])
+        if (currentUser) {
+            localStorage.setItem("user", JSON.stringify(currentUser));
+        } else {
+            localStorage.removeItem("user");
+        }
+    }, [currentUser]);
 
     return (
         <AuthContext.Provider value={{ currentUser, login, logout }}>
-          {children}
+            {children}
         </AuthContext.Provider>
     );
-}
+};
